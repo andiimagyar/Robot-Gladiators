a3d8fe2e6542d5f892dcbe835a77c44c6e405e6d
Andii Magyar <MacDaddy@Andiis-Mac-Pro.local>
2021-02-11 19:57:31 -0700

    Skip and Money for Win

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,18 +17,33 @@ var enemyAttack = 12;
 
 var fight = function (enemyName) {
 
-    while(enemyHealth > 0) {
+    while(enemyHealth > 0 && playerHealth > 0) {
     
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle. Enter 'FIGHT' or 'SKIP' to choose.");
 
-    if(promptFight === "fight" || promptFight === "FIGHT") {
+    if (promptFight === "skip" || promptFight === "SKIP") {
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+        
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            playerMoney = playerMoney - 10;
+            console.log ("playerMoney", playerMoney)
+            break;
+        }
+    }
+
         enemyHealth = enemyHealth - playerAttack;
         console.log (
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-        );
+        )
     
+    
+
     if (enemyHealth <= 0) {
-        window.alert(enemyName + " has died!"); }
+        window.alert(enemyName + " has died!"); 
+        playerMoney = playerMoney + 20;
+        break; 
+    }
          else {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
@@ -36,29 +51,20 @@ var fight = function (enemyName) {
     playerHealth = playerHealth - enemyAttack;
     console.log (
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    );
+    )
 
     if (playerHealth <= 0) {
         window.alert(playerName + " has died!");
+        break;
         } else {
             window.alert(playerName + " still has " + playerHealth + " health left.");
         }
         
-    } else if (promptFight === "skip" || promptFight === "SKIP") {
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-        if (confirmSkip) {
-            window.alert(playerName + " has decided to skip this fight. Goodbye!");
-            playerMoney = playerMoney - 2;
-        }
-    } else {
-    }
-}
+    } 
 };
 
 for (var i=0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
-    
-
 }
